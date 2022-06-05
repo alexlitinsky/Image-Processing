05a4724bb866df2c40bcccec3d3b88b863743ebd
bedrock <rollo.l@northeastern.edu>
2022-06-05 17:01:53 -0400

    Changed readPPM() to return an Image

--- ImageProcessing/src/ImageUtil.java
+++ ImageProcessing/src/ImageUtil.java
@@ -13,17 +13,17 @@ public class ImageUtil {
 
   /**
    * Read an image file in the PPM format and print the colors.
-   *
-   * @param filename the path of the file.
+   * @param filename the name of the file being read
+   * @return the image read from the PPM file
+   * @throws IllegalArgumentException if no file with the given name can be found
    */
-  public static void readPPM(String filename) {
+  public static Image readPPM(String filename) {
     Scanner sc;
 
     try {
       sc = new Scanner(new FileInputStream(filename));
     } catch (FileNotFoundException e) {
-      System.out.println("File " + filename + " not found!");
-      return;
+      throw new IllegalArgumentException("Filename " + filename + "not found!");
     }
     StringBuilder builder = new StringBuilder();
     //read the file line by line, and populate a string. This will throw away any comment lines
@@ -61,6 +61,8 @@ public class ImageUtil {
         System.out.println("Color of pixel (" + j + "," + i + "): " + r + "," + g + "," + b);
       }
     }
+
+    return image;
   }
 
   //demo main
