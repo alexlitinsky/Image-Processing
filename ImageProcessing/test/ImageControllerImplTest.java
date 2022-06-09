349d9e72d219631bd709fa7813d0e7e54393c0d0
bedrock <rollo.l@northeastern.edu>
2022-06-09 10:48:37 -0400

    added command testing - still incomplete

--- ImageProcessing/test/ImageControllerImplTest.java
+++ ImageProcessing/test/ImageControllerImplTest.java
@@ -2,6 +2,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
@@ -38,8 +39,8 @@ public class ImageControllerImplTest {
     this.input = new StringReader("load Images/Koala.ppm koala");
     this.controller = new ImageControllerImpl(input);
     controller.playGame();
-    assertEquals(768, controller.getVersions().get("koala").getDimensions()[0]);
-    assertEquals(1024, controller.getVersions().get("koala").getDimensions()[1]);
+    assertEquals(1024, controller.getVersions().get("koala").getDimensions()[0]);
+    assertEquals(768, controller.getVersions().get("koala").getDimensions()[1]);
     assertEquals(1, controller.getVersions().size());
     assertTrue(controller.getVersions().containsKey("koala"));
   }
@@ -50,22 +51,58 @@ public class ImageControllerImplTest {
    */
   @Test
   public void testBlueComponent() {
-//    this.input = new StringReader("load Images/Koala.ppm koala \n" +
-//            "blue-component koala koala-blue");
-    // no clue how to test this...
     this.input = new StringReader("load Images/koala-blue-greyscale.png koala-blue \n" +
             "load Images/Koala.ppm koala \n blue-component koala testBlue");
     this.controller = new ImageControllerImpl(input);
     controller.playGame();
-    ImageTextView view1 = new ImageTextView(controller.getVersions().get("testBlue"),
+    ImageTextView koalaView = new ImageTextView(controller.getVersions().get("koala"),
             new StringBuilder());
-    ImageTextView view2 = new ImageTextView(controller.getVersions().get("koala-blue"),
+    ImageTextView testBlueView = new ImageTextView(controller.getVersions().get("testBlue"),
             new StringBuilder());
-    //assertEquals(view2, view1);
-    //System.out.println(controller.getVersions().get("koala-blue"));
-    //this.input = new StringReader("blue-component koala koala-blue");
-    //controller.playGame();
-    //fail();
+    ImageTextView koalaBlueView = new ImageTextView(controller.getVersions().get("koala-blue"),
+            new StringBuilder());
+    assertNotEquals(koalaView.toString(), testBlueView.toString());
+    assertEquals(testBlueView.toString(), koalaBlueView.toString());
+  }
+
+  /**
+   * Method to test the brighten command.
+   * Should set all the RGB values of the image to the sum of the current value and the given value.
+   */
+  @Test
+  public void testBrighten() {
+    this.input = new StringReader("load Images/koala-brighter-by-50.png koala-brighter \n" +
+            "load Images/Koala.ppm koala \n brighten 50 koala testBrighter");
+    this.controller = new ImageControllerImpl(input);
+    controller.playGame();
+    ImageTextView koalaView = new ImageTextView(controller.getVersions().get("koala"),
+            new StringBuilder());
+    ImageTextView testBrightenView = new ImageTextView(controller.getVersions().get("testBrighter"),
+            new StringBuilder());
+    ImageTextView brightView = new ImageTextView(controller.getVersions().get("koala-brighter"),
+            new StringBuilder());
+    assertNotEquals(koalaView.toString(), testBrightenView.toString());
+    assertEquals(testBrightenView.toString(), brightView.toString());
+  }
+
+  /**
+   * Method to test the flip horizontal command.
+   * Should move all the pixels on the left half of the picture to the right & vice versa.
+   */
+  @Test
+  public void testFlipHorizontal() {
+    this.input = new StringReader("load Images/koala-horizontal.png koala-horizontal \n" +
+            "load Images/Koala.ppm koala \n horizontal-flip koala testHorizontal");
+    this.controller = new ImageControllerImpl(input);
+    controller.playGame();
+    ImageTextView koalaView = new ImageTextView(controller.getVersions().get("koala"),
+            new StringBuilder());
+    ImageTextView testHorizontal = new ImageTextView(controller.getVersions().get("testHorizontal"),
+            new StringBuilder());
+    ImageTextView horizontal = new ImageTextView(controller.getVersions().get("koala-horizontal"),
+            new StringBuilder());
+    assertNotEquals(koalaView.toString(), testHorizontal.toString());
+    assertEquals(testHorizontal.toString(), horizontal.toString());
   }
 
   /**
