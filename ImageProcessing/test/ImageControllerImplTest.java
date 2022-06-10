77084a76c64422eb6eae720dc3656a2abb02207f
bedrock <rollo.l@northeastern.edu>
2022-06-09 20:49:44 -0400

    Added documentation 2

--- ImageProcessing/test/ImageControllerImplTest.java
+++ ImageProcessing/test/ImageControllerImplTest.java
@@ -8,7 +8,6 @@ import view.ImageTextView;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
--- ImageProcessing/test/PixelTest.java
+++ ImageProcessing/test/PixelTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
+
 import model.Pixel;
+
 import static org.junit.Assert.assertEquals;
 
 /**
