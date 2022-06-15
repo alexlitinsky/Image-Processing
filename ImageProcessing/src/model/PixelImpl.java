e124fa2e73b57e58ad40b72c59b9cac1f12f2eb3
bedrock <rollo.l@northeastern.edu>
2022-06-15 18:24:34 -0400

    Added testing for new PixelImpl methods, as well as changed applyToAll() to call the other applyTo() methods in PixelImpl

--- ImageProcessing/src/model/PixelImpl.java
+++ ImageProcessing/src/model/PixelImpl.java
@@ -36,19 +36,39 @@ public class PixelImpl implements Pixel {
     return new int[]{this.red, this.green, this.blue};
   }
 
+  /**
+   * Multiplies the RGB values of this pixel by the given value. Returns 0 or 255 if the result is < 0 or > 255.
+   * @param val the value being applied
+   * @return double to represent the product of the RGB values and the specified value
+   */
   @Override
   public double[] applyToAll(double val) {
-    return new double[] { this.red * val, this.green * val, this.blue * val};
+    return new double[] { applyToR(val), applyToG(val), applyToB(val)};
   }
 
+  /**
+   * Multiplies the red value by the given value. Returns 0 or 255 if the result is < 0 or > 255.
+   * @param val the value being applied
+   * @return double to represent the product of the red value and the specified value
+   */
   @Override
   public double applyToR(double val) { return Math.max(Math.min(this.red * val, 255), 0); }
 
+  /**
+   * Multiplies the green value by the given value. Returns 0 or 255 if the result is < 0 or > 255.
+   * @param val the value being applied
+   * @return double to represent the product of the green value and the specified value
+   */
   @Override
   public double applyToG(double val) { return Math.max(Math.min(this.green * val, 255), 0);
   }
 
+  /**
+   * Multiplies the blue value by the given value. Returns 0 or 255 if the result is < 0 or > 255.
+   * @param val the value being applied
+   * @return double to represent the product of the blue value and the specified value
+   */
   @Override
-  public double applyToB(double val) { return Math.max(Math.min(this.red * val, 255), 0); }
+  public double applyToB(double val) { return Math.max(Math.min(this.blue * val, 255), 0); }
 
 }
--- ImageProcessing/test/PixelImplTest.java
+++ ImageProcessing/test/PixelImplTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import model.Pixel;
 import model.PixelImpl;
 
 import static org.junit.Assert.assertEquals;
@@ -54,5 +55,59 @@ public class PixelImplTest {
     assertEquals(0, p.getRGB()[0]);
     assertEquals(1, p.getRGB()[1]);
     assertEquals(2, p.getRGB()[2]);
+  }
+
+  /**
+   * Tests the applyToAll method. Should change all RGB values according to the specified value.
+   * Should only return values between 0 and 255.
+   */
+  @Test
+  public void testApplyToAll() {
+    Pixel p = new PixelImpl(1, 3, 2);
+    assertEquals(2.1, p.applyToAll(2.1)[0], 0);
+    assertEquals(255, p.applyToAll(256)[0], 0);
+    assertEquals(0, p.applyToAll(-1)[0], 0);
+    assertEquals(6, p.applyToAll(2.0)[1], 0);
+    assertEquals(4, p.applyToAll(2.0)[2], 0);
+    assertEquals(0, p.applyToAll(0.0)[2], 0);
+  }
+
+  /**
+   * Test applyToR(). Should return the red value multiplied by the given value.
+   * Should only return values between 0 and 255.
+   */
+  @Test
+  public void testApplyToR() {
+    Pixel p = new PixelImpl(1, 3, 2);
+    assertEquals(2.1, p.applyToR(2.1), 0);
+    assertEquals(255, p.applyToR(256), 0);
+    assertEquals(0.0, p.applyToR(0.0), 0);
+    assertEquals(0.0, p.applyToR(-1), 0);
+  }
+
+  /**
+   * Test applyToG(). Should return the green value multiplied by the given value.
+   * Should only return values between 0 and 255.
+   */
+  @Test
+  public void testApplyToG() {
+    Pixel p = new PixelImpl(1, 4, 2);
+    assertEquals(8.4, p.applyToG(2.1), 0);
+    assertEquals(255, p.applyToG(256), 0);
+    assertEquals(0.0, p.applyToG(0.0), 0);
+    assertEquals(0, p.applyToG(-1), 0);
+  }
+
+  /**
+   * Test applyToB(). Should return the blue value multiplied by the given value.
+   * Should only return values between 0 and 255.
+   */
+  @Test
+  public void testApplyToB() {
+    Pixel p = new PixelImpl(1, 3, 2);
+    assertEquals(4.2, p.applyToB(2.1), 0);
+    assertEquals(255, p.applyToB(256), 0);
+    assertEquals(0.0, p.applyToB(0.0), 0);
+    assertEquals(0.0, p.applyToB(-1), 0);
   }
 }
